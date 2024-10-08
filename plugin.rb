e586da7b6a468b812497e9e229c4214e4ab511cc
kendywetalk <127364763+kendywetalk@users.noreply.github.com>
2024-10-08 18:06:23 +0700

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -7,7 +7,7 @@ module ::CaseSearch
 
     def search_case_number
       case_number = params[:case_number]
-      uri = URI("http://localhost:5000/search?case_number=#{URI.encode(case_number)}")
+      uri = URI("http://localhost:5000/search?case_number=#{URI.encode_www_form_component(case_number)}")
       response = Net::HTTP.get(uri)
       result = JSON.parse(response)
 
@@ -20,29 +20,16 @@ module ::CaseSearch
   end
 end
 
-discourse_plugin_registry.register_route(:get, '/case-search', 'CaseSearch::SearchController', :search_case_number)
+# Registering route to the controller
+Discourse::Application.routes.append do
+  get '/case-search' => 'case_search/search#search_case_number'
+end
 
-# Modal Template for Search Results
-discoursePluginRegistry.registerTemplate('case-search-result', {
-  componentName: 'modal-base',
-  title: 'Case Search Result',
-  body(model) {
-    if (model.message) {
-      return `<p>${model.message}</p>`;
-    }
-    return `
-      <div>
-        <h4>Case Number: ${model.Case_Number}</h4>
-        <p><strong>Priority Date:</strong> ${model.Priority_Date}</p>
-        <p><strong>Draft Date:</strong> ${model.Draft_Date}</p>
-        <p><strong>Audit Date:</strong> ${model.Audit_Date}</p>
-        <p><strong>Result Date:</strong> ${model.Result_Date}</p>
-        <p><strong>Status:</strong> ${model.Status}</p>
-        <p><strong>Days to Result:</strong> ${model.Days_To_Result}</p>
-        <p><strong>Days Pending:</strong> ${model.Days_Pending}</p>
-        <p><strong>Employer Name:</strong> ${model.Employer_Name}</p>
-        <p><strong>Job Title:</strong> ${model.Job_Title}</p>
-      </div>
-    `;
-  }
-});
+# JavaScript and modal template registration
+register_asset 'javascripts/case-search.js.es6'
+
+after_initialize do
+  Discourse::Application.routes.append do
+    get '/case-search' => 'case_search/search#search_case_number'
+  end
+end
